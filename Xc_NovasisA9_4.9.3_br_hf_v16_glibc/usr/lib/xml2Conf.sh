cd09687ba7c429b232bebbc228344db7ae6387a0
NovasomIndustries <filippo.visocchi@novasomindustries.com>
2016-11-02 15:47:12 +0100

    Initial commit

---
+++ Xc_NovasisA9_4.9.3_br_hf_v16_glibc/usr/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Devel/NOVAsom_SDK/Xcompiler/Xc_NovasisA9_4.9.3_br_hf_v16_glibc/usr/lib"
+XML2_LIBS="-lxml2      -lm "
+XML2_INCLUDEDIR="-I/Devel/NOVAsom_SDK/Xcompiler/Xc_NovasisA9_4.9.3_br_hf_v16_glibc/usr/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
